6d5866080b8e93443d19fbe9238fa06d353ada5b
MargineanMiruna-Elena <miruna.marginean.2018@gmail.com>
2025-07-16 11:28:00 +0300

    dependency injection modifications

--- simple-spring-project/src/main/java/com/example/App.java
+++ simple-spring-project/src/main/java/com/example/App.java
@@ -1,12 +1,12 @@
 package com.example;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MyBean myBean = context.getBean("myBean", MyBean.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MyBean myBean = context.getBean(MyBean.class);
         myBean.sayHello();
     }
 }
