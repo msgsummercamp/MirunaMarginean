c66a08623e0997cab871b9473f52ddb200ff3936
MargineanMiruna-Elena <miruna.marginean.2018@gmail.com>
2025-07-23 11:49:27 +0300

    changes based on review

--- first-angular-project/src/app/components/navbar/navbar.component.ts
+++ first-angular-project/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { MatToolbar } from '@angular/material/toolbar';
 import { MatAnchor, MatIconButton } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbar, MatIcon, MatIconButton, MatAnchor, RouterLink, MatIcon, MatIcon],
+  imports: [MatToolbar, MatIcon, MatIconButton, MatAnchor, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
